17be77b9db5bd5f494b4b3b97176307145e03b83
Dov Rine <dov@techimpact.org>
2021-08-12 13:14:52 -0700

    show first question with answers

--- modules/game.js
+++ modules/game.js
@@ -1,6 +1,29 @@
+import { getQuestions } from "./api.js";
+
 var questions = [];
 var currentQuestion = 0;
 
+function hideForm() {
+  const form = document.querySelector("#questions-form");
+  form.classList.add("hide");
+}
+function setGameQuestions(rawQuestions) {
+  questions = rawQuestions.map((q) => ({
+    question: q.question,
+    answers: shuffle([q.correct_answer, ...q.incorrect_answers]),
+    correct: q.correct_answer,
+  }));
+}
+function showCurrentQuestion() {
+  const questionSection = document.querySelector("#question");
+  const { question, answers } = questions[currentQuestion];
+  questionSection.querySelector(".question").innerHTML = question;
+  const answerElements = questionSection.querySelectorAll(".answer");
+  for (let i = 0; i < answers.length; i++) {
+    answerElements[i].innerHTML = answers[i];
+  }
+  questionSection.classList.remove("hide");
+}
 function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -8,15 +31,10 @@ function shuffle(arr) {
   }
   return arr;
 }
-
-
 function startGame(rawQuestions) {
-  questions = rawQuestions.map((q) => ({
-    question: q.question,
-    answers: shuffle([q.correct_answer, ...q.incorrect_answers]),
-    correct: q.correct_answer,
-  }));
-  console.log(questions);
+  setGameQuestions(rawQuestions);
+  hideForm();
+  showCurrentQuestion();
 }
 
 export { startGame };
