c1deb43f3a25225dda222c2a94c5521d8b3cf396
Dov Rine <dov@techimpact.org>
2021-08-12 11:40:45 -0700

    shuffle answers array to make game unpredictable

--- modules/game.js
+++ modules/game.js
@@ -1,14 +1,22 @@
 var questions = [];
-var currentQuestion = 0
+var currentQuestion = 0;
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
 
 function startGame(rawQuestions) {
-  questions = rawQuestions.map(q => ({
+  questions = rawQuestions.map((q) => ({
     question: q.question,
-    answers: [q.correct_answer, ...q.incorrect_answers],
+    answers: shuffle([q.correct_answer, ...q.incorrect_answers]),
     correct: q.correct_answer,
-})
-  )
-  console.log(questions)
+  }));
+  console.log(questions);
 }
 
 export { startGame };
